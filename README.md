5035f4d5f1fbdce3a8897afe7c8ae83aae151d98
vinothrobinson <vinothrobinson03@gmail.com>
2023-09-03 11:38:24 -0400

    Added Prettier, and stylized the code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # weather-app
+
 This is a JavaScript implementation of a Weather App which utilizes the Weather API to extract weather information.
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { apiKey } from "./apiKey";
 
 async function getWeather() {
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=mississauga`, {mode: "cors"});
-    const weatherData = await response.json();
-    console.log(weatherData);
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=mississauga`,
+    { mode: "cors" },
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
 }
 
 getWeather();
