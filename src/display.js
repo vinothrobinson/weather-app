0fc594aa3bfc9da4260e62be41eb60d3e4feee59
vinothrobinson <vinothrobinson03@gmail.com>
2023-09-04 14:54:27 -0400

    Added a display for hourly weather

--- src/display.js
+++ src/display.js
@@ -1,4 +1,5 @@
-import { forecastInfo } from "./index.js"
+import { forecastInfo, hourlyInfo, nightTime, dayTime } from "./index.js"
+import { dayIcons, nightIcons } from "./weatherIcons.js"
 
 function displayDaily() {
     let dailyWeather = document.querySelector(".forecast")
@@ -8,7 +9,43 @@ function displayDaily() {
         days[i].querySelector(".maxTemp").textContent = forecastInfo[i].max_temp
         days[i].querySelector(".minTemp").textContent = forecastInfo[i].min_temp
         days[i].querySelector(".weather").textContent = forecastInfo[i].weather
+        days[i].querySelector("img").src = dayIcons[forecastInfo[i].weather]
     }
 }
 
-export {displayDaily};
+let switchScreen = document.querySelector(".buttons").children;
+switchScreen[0].addEventListener("click", function(){
+    let counter = 0
+    displayHourly(counter)
+})
+
+switchScreen[1].addEventListener("click", function(){
+    let counter = 8
+    displayHourly(counter)
+})
+
+switchScreen[2].addEventListener("click", function(){
+    let counter = 16
+    displayHourly(counter)
+})
+
+function displayHourly(counter) {
+    let hourlyWeather = document.querySelector(".hourly")
+    let hours = hourlyWeather.children
+    console.log(dayTime)
+    console.log(nightTime)
+    for (let i = 0; i < hours.length; i++) {
+        hours[i].querySelector(".hour").textContent = hourlyInfo[counter].time
+        hours[i].querySelector(".temp").textContent = hourlyInfo[counter].temp
+        hours[i].querySelector(".weather").textContent = hourlyInfo[counter].weather
+        
+        if (counter < dayTime || counter > nightTime) {
+            hours[i].querySelector("img").src = nightIcons[hourlyInfo[counter].weather]
+        } else {
+            hours[i].querySelector("img").src = dayIcons[hourlyInfo[counter].weather]
+        }
+        
+        counter++;
+    }
+}
+export {displayDaily, displayHourly};
